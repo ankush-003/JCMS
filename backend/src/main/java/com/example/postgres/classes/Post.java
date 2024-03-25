e5b3e72dad9be4cea302bfb536d417c5e9bf25cb
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2024-03-26 00:04:07 +0530

    Added Builder Pattern

--- backend/src/main/java/com/example/postgres/classes/Post.java
+++ backend/src/main/java/com/example/postgres/classes/Post.java
@@ -3,6 +3,7 @@ package com.example.postgres.classes;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name = "posts", schema = "public")
 public class Post {
     @Id
