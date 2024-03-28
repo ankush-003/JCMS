5fe5a8bb9bfc2ba3f19e9b68d06464c618743bf0
Anshul Dasyam <92244985+Anshul1563@users.noreply.github.com>
2024-03-29 01:56:48 +0530

    Add @JsonBackReference and @JsonManagedReference annotations

---
+++ backend/src/main/java/com/example/postgres/controller/PostController.java
@@ -0,0 +1,18 @@
+package com.example.postgres.controller;
+
+import com.example.postgres.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+}
