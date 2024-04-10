6e9c6f12f7c396218f4089a658e323a212d51b2b
Arya Vinayak R <aryavinayak2003@gmail.com>
2024-04-10 11:51:12 +0530

    fixes

--- backend/src/main/java/com/example/postgres/Dto/TokenType.java
+++
@@ -1,5 +0,0 @@
-package com.example.postgres.Dto;
-
-public enum TokenType {
-    Bearer
-}
--- backend/src/main/java/com/example/postgres/repository/ChannelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.postgres.repository;
-
-import com.example.postgres.classes.Channel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChannelRepository extends JpaRepository<Channel, Long> {
-}
