d7d3e3e79a2a305169cc8145322ef82507936115
Arya Vinayak R <aryavinayak2003@gmail.com>
2024-04-10 11:51:12 +0530

    fixes

--- backend/src/main/java/com/example/postgres/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.postgres.repository;
-
-import com.example.postgres.classes.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
--- backend/src/main/java/com/example/postgres/repository/VoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.postgres.repository;
-
-import com.example.postgres.classes.Vote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-}
