6db15e30d8d5f1dafceee547d87d201e2b3a7dbf
AGJoshi1729 <anirudhgj2003@gmail.com>
2024-04-15 09:42:14 +0530

    Delete frontend folder

--- frontend/src/main/java/com/example/application/repository/ChannelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.application.repository;
-
-import com.example.postgres.classes.Channel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChannelRepository extends JpaRepository<Channel, Long> {
-}
