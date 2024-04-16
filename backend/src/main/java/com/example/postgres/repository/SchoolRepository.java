c7751ce594e3a5c61170104f9da841fed1723ee7
AGJoshi1729 <anirudhgj2003@gmail.com>
2024-04-16 21:34:42 +0530

    Delete unwanted files

--- backend/src/main/java/com/example/postgres/repository/SchoolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.postgres.repository;
-
-import com.example.postgres.classes.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School,Integer> {
-}
