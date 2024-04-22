940cf22fd06c64f2b7a8e13c7c696e11141ff745
AGJoshi1729 <anirudhgj2003@gmail.com>
2024-04-23 02:45:58 +0530

    Refactor ChannelView

--- backend/src/main/java/com/example/postgres/view/list/ChannelView.java
+++ backend/src/main/java/com/example/postgres/view/list/ChannelView.java
@@ -257,10 +257,6 @@ public class ChannelView extends Div implements BeforeEnterObserver {
         return commentSection;
     }
 
-
-        add(createPostButton);
-    }
-
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         Optional<String> channelNameParam = event.getRouteParameters().get("channelName");
